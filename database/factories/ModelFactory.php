a4b78021c92941bdd4f85d4675b4d3b4798f0567
장 성현 <jeom0427@naver.com>
2019-12-03 11:27:32 +0000

    Revert "지나가는"
    This reverts commit a8f5b9660320f2481e17511d24b12d68aee7d212.

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\User;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\User::class, function (Faker $faker) {
+    return [
+        'email' => $faker->unique()->safeEmail,
+        'password' => bcrypt('oomori'),
+        'name' => $faker->name,
+        'birth' => now(),
+        'gender' => 'slave',
+        'activated' => '1',
+    ];
+});
+
+$factory->define(App\Article::class, function (Faker $faker) {
+    return [
+        'article_type' => '아나까나',
+        'title' => $faker->sentence(),
+        'content' => $faker->paragraph()
+    ];
+});
