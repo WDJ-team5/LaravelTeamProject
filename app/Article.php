a8f5b9660320f2481e17511d24b12d68aee7d212
장 성현 <jeom0427@naver.com>
2019-12-03 11:22:10 +0000

    지나가는

--- app/Article.php
+++ app/Article.php
@@ -7,10 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $fillable = ['title', 'content', 'article_type'];
+	
+	public function attachments(){
+		return $this->hasMany(Attachment::class);
+	}
 
     public function user()
     {
         return $this->belongsTo('App\User');
     }
+	
+	public function getBytesAttribute($value){
+		return format_filesize($value);
+	}
 
 }
