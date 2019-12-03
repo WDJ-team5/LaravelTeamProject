486316bcc53f197b7a04161f8e01e1afd1164e04
장현호 <jhh9507@gmail.com>
2019-12-03 08:13:37 +0000

    내가 쓴글  기초작업

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -6,6 +6,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
     <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>  
 </head>
 <body>
     
@@ -74,12 +79,6 @@
     </div>
 </div>
     
-
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
-    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>  
 <script type="text/javascript">
 
 
@@ -95,10 +94,6 @@
         processing: true,
         serverSide: true,
         ajax: "{{ route('articles.index') }}",
-		// ajax: {
-		// 	"{{ route('articles.index') }}",
-		// 	"dataType": "json"
-		// }
 		columns: [
             {data: 'DT_RowIndex', name: 'DT_RowIndex'},
             {data: 'title', name: 'title'},
---
+++ resources/views/myarticle.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('script')
+
+@endsection
+
+@section('style')
+    
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+			
+            여기다 ㄱㄱ~
+        </div>
+    </div>
+</div>
+@endsection
