fd294276359482353eae33d090f054080aae31b1
장현호 <jhh9507@gmail.com>
2019-12-03 20:28:29 +0000

    QnA Ajax CRUD  완료

--- app/Http/Controllers/QnAsController.php
+++ app/Http/Controllers/QnAsController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Article; 
 use DataTables;
 
 class QnAsController extends Controller
@@ -12,15 +11,15 @@ class QnAsController extends Controller
     public function index(Request $request)
     {
 		if ($request->ajax()) {
-			$data = \App\User::join('articles', 'users.id', '=', 'articles.user_id')->where('article_type','QnA')->get();
+			$data = \App\User::join('articles', 'users.id', '=', 'articles.user_id')->where('article_type','QnA')->orderBy('articles.id','desc')->get();
 			return Datatables::of($data)
 				->addIndexColumn()
 				->addColumn('action', function($row){
-					$btn = '<div class="text-center"><a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="show" class="btn btn-primary btn-sm showQnA">읽기</a>';
+					$btn = '<div class="text-center"><a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="show" class="btn btn-primary btn-sm showQnA">Show</a>';
 					$btn = $btn.'<span>&nbsp;&nbsp;</span>';
-					$btn = $btn.'<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Edit" class="edit btn btn-primary btn-sm editQnA">수정</a>';
+					$btn = $btn.'<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Edit" class="edit btn btn-success btn-sm editQnA">Edit</a>';
 					$btn = $btn.'<span>&nbsp;&nbsp;</span>';
-					$btn = $btn.'<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Delete" class="btn btn-danger btn-sm deleteQnA">삭제</a></div>';
+					$btn = $btn.'<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Delete" class="btn btn-danger btn-sm deleteQnA">Delete</a></div>';
 					return $btn;
 				})
 				->rawColumns(['action'])
@@ -30,41 +29,51 @@ class QnAsController extends Controller
         return view('qna',compact('article'));
     }
 
-    public function create()
-    {
+	public function create()
+	{
 
-    }
+	}
 
-    //게시글 저장
-    public function store(Request $request)
-    {        
-        auth()->user()->articles()->updateOrCreate(['id' => $request->article_id], 
-        ['article_type' => 'QnA', 'title' => $request->title, 'content' => $request->content]);
-        
-        return response()->json(['success'=>'Article saved successfully.']);
-    }
+	public function store(Request $request)
+	{
+		auth()->user()->articles()->create([
+			'article_type' => 'QnA',
+			'title' => $request->title,
+			'content' => $request->content
+		]);
 
-    public function show($id)
-    {
-        $article = Article::find($id);
-        return response()->json($article);
-    }
+		return response()->json(['success'=>'Article saved successfully.']);
+	}
 
-    public function edit($id)
-    {
-        $article = Article::find($id);
-        return response()->json($article);
-    }
+	public function show($id)
+	{
+		$article = \App\Article::find($id);
+		
+		return response()->json($article);
+	}
 
-    public function update(Request $request, $id)
-    {
-        //
-    }
+	public function edit($id)
+	{
+		$article = \App\Article::find($id);
+		
+		return response()->json($article);
+	}
 
-    public function destroy($id)
-    {
-        Article::find($id)->delete();
-     
-        return response()->json(['success'=>'Article deleted successfully.']);
-    }
+	public function update(Request $request, $id)
+	{
+		\App\Article::find($id)->update([
+			'article_type' => 'QnA',
+			'title' => $request->title,
+			'content' => $request->content
+		]);
+		
+		return response()->json(['success'=>'Article updated successfully.']);
+	}
+
+	public function destroy($id)
+	{
+		\App\Article::find($id)->delete();
+
+		return response()->json(['success'=>'Article deleted successfully.']);
+	}
 }
--- resources/views/qna.blade.php
+++ resources/views/qna.blade.php
@@ -2,119 +2,134 @@
 
 @section('script')
 <script type="text/javascript">
-
-
-  $(function () {
-      $.ajaxSetup({
-          headers: {
-              'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-          }
-    });
-
-
-    var table = $('.data-table').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: "{{ route('qnas.index') }}",
-		order: [[1, "desc"]],
-		columns: [
-            {data: 'id', name: 'id',},
-            {data: 'title', name: 'title'},
-			{data: 'name', name: 'name'},
-			{data: 'created_at', name: 'created_at'},
-            {data: 'action', name: 'action', orderable: false, searchable: false},
-        ]
-    });
-
-
-	  // 새 글쓰기 버튼 클릭시
-    $('#createNewQnA').click(function () {
-        $('#saveBtn').val("create-QnA");
-        $('#QnA_id').val('');
-        $('#QnAForm').trigger("reset");
-        $('#modelHeading').html("Create New QnA");
-        $('#ajaxModel01').modal('show');
-    });
-
-
-	  // 수정 버튼 클릭시
-    $('body').on('click', '.editQnA', function () {
-      var QnA_id = $(this).data('id');
-      $.get("{{ route('qnas.index') }}" +'/' + QnA_id +'/edit', function (data) {
-          $('#modelHeading').html("글 수정하기");
-          $('#saveBtn').val("edit-QnA");
-		  $('#ajaxModel01').modal('show');
-          $('#QnA_id').val(data.id);
-          $('#title').val(data.title);
-          $('#content').val(data.content);
-      })
-   });
-
-
-	  // 게시글 저장 버튼 클릭시 
-    $('#saveBtn').click(function (e) {
-        e.preventDefault();
-        $(this).html('Save');
-    
-		$.ajax({
-			data: $('#QnAForm').serialize(),
-			url: "{{ route('qnas.store') }}",
-			type: "POST",
-			dataType: 'json',
-			success: function (data) {
-
-			  $('#QnAForm').trigger("reset");
-			  $('#ajaxModel01').modal('hide');
-			  table.draw();
-
-			},
-
-			error: function (data) {
-			  console.log('Error:', data);
-			  $('#saveBtn').html('저장실패');
-			}
-		});
-    });
-    
-
-
-    $('body').on('click', '.deleteQnA', function () {
-     
-        var QnA_id = $(this).data("id");
-        confirm("삭제하시겠습니까?");
-      
-        $.ajax({
-            type: "DELETE",
-            url: "{{ route('qnas.store') }}"+'/'+QnA_id,
-            success: function (data) {
-                table.draw();
-            },
-            error: function (data) {
-                console.log('Error:', data);
-            }
-        });
-    });
-	  
-	$('body').on('click', '.showQnA', function () {
-      var QnA_id = $(this).data('id');
-      $.get("{{ route('qnas.index') }}" +'/' + QnA_id, function (data) {
-          $('#modelHeading2').html("글 읽는 즁");
-		  $('#ajaxModel02').modal('show');
-          $('#QnA_id2').val(data.id);
-          $('#title2').text(data.title);
-          $('#content2').text(data.content);
-      })
-   });
 	
-	  
-	  $('.fa-times').click(function() {
-		 $('#ajaxModel01').modal('hide');
-		 $('#ajaxModel02').modal('hide');
-	  });
-	 
-  });
+	$(function () {
+		
+		/* CSRF 상태 토큰 */
+		$.ajaxSetup({
+			headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+		}});
+
+		/* table ajax index */
+		var table = $('.data-table').DataTable({
+			processing: true,
+			serverSide: true,
+			ajax: "{{ route('qnas.index') }}",
+			columns: [
+				{data: 'id', name: 'id', orderable: false},
+				{data: 'title', name: 'title'},
+				{data: 'name', name: 'name'},
+				{data: 'created_at', name: 'created_at'},
+				{data: 'action', name: 'action', orderable: false, searchable: false},
+			]
+		});
+
+		/* 새 글 쓰기 버튼 클릭 시 */
+		$('#createNewQnA').click(function () {
+			$('#saveBtn').text("Save");
+			$('#QnA_id').val('');
+			$('#QnAForm').trigger("reset");
+			$('#modelHeading').html("Create New QnA");
+			$('#ajaxModel01').modal('show');
+		});
 
 
+		/* table ajax edit */
+		$('body').on('click', '.editQnA', function () {
+			$('#saveBtn').text("Update");
+			var QnA_id = $(this).data('id');
+			$.get("{{ route('qnas.index') }}" + '/' + QnA_id + '/edit', function (data) {
+				$('#modelHeading').html("글 수정하기");
+				$('#saveBtn').val("edit-QnA");
+				$('#ajaxModel01').modal('show');
+				$('#QnA_id').val(data.id);
+				$('#title').val(data.title);
+				$('#content').val(data.content);
+			})
+		});
+
+		/* table ajax create & update */
+		$('#saveBtn').click(function (e) {
+			e.preventDefault();
+			
+			if($('#saveBtn').text() == 'Save') {
+				$(this).html('Saving...');
+				$.ajax({
+					data: $('#QnAForm').serialize(),
+					url: "{{ route('qnas.store') }}",
+					type: "POST",
+					dataType: 'json',
+					success: function (data) {
+						$('#QnAForm').trigger("reset");
+						$('#ajaxModel01').modal('hide');
+						table.draw();
+					},
+					error: function (data) {
+						console.log('Error:', data);
+						$('#saveBtn').html('실패');
+					}
+				});
+			}else {
+				var QnA_id = $('#QnA_id').val();
+				$(this).html('Udating...');
+				$.ajax({
+					data: $('#QnAForm').serialize(),
+					type: "PUT",
+					url: "{{ route('qnas.store') }}"+'/'+QnA_id,
+					dataType: 'json',
+					success: function (data) {
+						$('#QnAForm').trigger("reset");
+						$('#ajaxModel01').modal('hide');
+						table.draw();
+					},
+					error: function (data) {
+						console.log('Error:', data);
+						$('#saveBtn').html('실패');
+					}
+				});
+			}
+			
+			
+		});
+
+
+		/* table ajax delete */
+		$('body').on('click', '.deleteQnA', function () {
+			var QnA_id = $(this).data("id");
+			var result = confirm("삭제하시겠습니까?");	
+			if(result) {
+				$.ajax({
+					type: "DELETE",
+					url: "{{ route('qnas.store') }}"+'/'+QnA_id,
+					success: function (data) {
+						table.draw();
+					},
+					error: function (data) {
+						console.log('Error:', data);
+					}
+				});
+			}   
+		});
+
+		/* table ajax show */
+		$('body').on('click', '.showQnA', function () {
+			var QnA_id = $(this).data('id');
+			$.get("{{ route('qnas.index') }}" +'/' + QnA_id, function (data) {
+				$('#modelHeading2').html("글 읽는 즁");
+				$('#ajaxModel02').modal('show');
+				$('#QnA_id2').val(data.id);
+				$('#title2').text(data.title);
+				$('#content2').text(data.content);
+			})
+		});
+
+		/* etc */
+		$('.fa-times').click(function() {
+			$('#ajaxModel01').modal('hide');
+			$('#ajaxModel02').modal('hide');
+		});
+
+	});
 </script>
 @endsection
 
@@ -161,74 +176,76 @@
 </div>
 <!-- Create&Edit modal -->
 <div class="modal fade" id="ajaxModel01" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-
-            <div class="modal-header">
-                <h4 class="modal-title" id="modelHeading"></h4>
+	<div class="modal-dialog">
+		<div class="modal-content">
+			
+			<div class="modal-header">
+				<h4 class="modal-title" id="modelHeading"></h4>
 				<div>
 					<i class="fas fa-times fa-3x"></i>
 				</div>
-            </div>
+			</div>
 
-            <div class="modal-body">
-                <form id="QnAForm" name="QnAForm" class="form-horizontal">
-                   <input type="hidden" name="QnA_id" id="QnA_id">
-					
-                   <!-- 제목 폼 -->
-                    <div class="form-group">
-                        <label for="name" class="col-sm-2 control-label">제목</label>
-                        <div class="col-sm-12">
-                            <input type="text" class="form-control" id="title" name="title" placeholder="Enter Title" value="" maxlength="50" required="">
-                        </div>
-                    </div>
-                    
-                    <!-- 본문 폼 -->
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label">본문</label>
-                        <div class="col-sm-12">
-                            <textarea id="content" name="content" required="" placeholder="Enter Content" class="form-control"></textarea>
-                        </div>
-                    </div>
+			<div class="modal-body">
+				<form id="QnAForm" name="QnAForm" class="form-horizontal">
+					<input type="hidden" name="QnA_id" id="QnA_id">
 
-                    <!-- 게시글 저장버튼 -->
-                    <div class="col-sm-offset-2 col-sm-10">
-                     <button type="submit" class="btn btn-primary" id="saveBtn" value="create">저장하기</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
+					<!-- 제목 폼 -->
+					<div class="form-group">
+						<label for="name" class="col-sm-2 control-label">제목</label>
+						<div class="col-sm-12">
+							<input type="text" class="form-control" id="title" name="title" placeholder="Enter Title" value="" maxlength="50" required="">
+						</div>
+					</div>
+
+					<!-- 본문 폼 -->
+					<div class="form-group">
+						<label class="col-sm-2 control-label">본문</label>
+						<div class="col-sm-12">
+							<textarea id="content" name="content" required="" placeholder="Enter Content" class="form-control"></textarea>
+						</div>
+					</div>
+
+					<!-- 게시글 저장&수정버튼 -->
+					<div class="col-sm-offset-2 col-sm-10">
+						<button type="submit" class="btn btn-primary" id="saveBtn" value="create">저장하기</button>
+					</div>
+				</form>
+			</div>
+			
+		</div>
+	</div>
 </div>
 <!-- Show modal -->
 <div class="modal fade" id="ajaxModel02" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
+	<div class="modal-dialog">
+		<div class="modal-content">
 
-            <div class="modal-header">
-                <h4 class="modal-title" id="modelHeading2"></h4>
+			<div class="modal-header">
+				<h4 class="modal-title" id="modelHeading2"></h4>
 				<div>
 					<i class="fas fa-times fa-3x"></i>
 				</div>
-            </div>
+			</div>
 
-            <div class="modal-body">
-                <form id="QnAForm" name="QnAForm" class="form-horizontal">
-                   <input type="hidden" name="QnA_id" id="QnA_id2">
+			<div class="modal-body">
+				<form id="QnAForm" name="QnAForm" class="form-horizontal">
+					<input type="hidden" name="QnA_id" id="QnA_id2">
 
-                   <!-- 제목 폼 -->
-                    <div class="form-group">
+					<!-- 제목 -->
+					<div class="form-group">
 						<h1 id="title2"></h1>
-                    </div>
-                    
-                    <!-- 본문 폼 -->
-                    <div class="form-group">
+					</div>
+
+					<!-- 본문 -->
+					<div class="form-group">
 						<p id="content2"></p>
 					</div>
-                </form>
-            </div>
-        </div>
-    </div>
+				</form>
+			</div>
+			
+		</div>
+	</div>
 </div>
 @endsection 
 
