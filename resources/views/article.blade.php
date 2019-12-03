ea871b73ee5790ceccd6fc2c2218f20a3ca2a434
장 성현 <jeom0427@naver.com>
2019-12-03 11:25:53 +0000

    Revert "내가 쓴글  기초작업"
    This reverts commit 486316bcc53f197b7a04161f8e01e1afd1164e04.

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -6,11 +6,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
     <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
-    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>  
 </head>
 <body>
     
@@ -76,6 +71,12 @@
     </div>
 </div>
     
+
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>  
 <script type="text/javascript">
 
 
@@ -91,6 +92,10 @@
         processing: true,
         serverSide: true,
         ajax: "{{ route('articles.index') }}",
+		// ajax: {
+		// 	"{{ route('articles.index') }}",
+		// 	"dataType": "json"
+		// }
 		columns: [
             {data: 'DT_RowIndex', name: 'DT_RowIndex'},
             {data: 'title', name: 'title'},
--- resources/views/layouts/partial/navigation.blade.php
+++ resources/views/layouts/partial/navigation.blade.php
@@ -19,7 +19,7 @@
                     <a class="nav-link" href="#">현지학기제</a>
                 </li>
 				<li class="nav-item">
-                    <a class="nav-link" href="{{ route('articles.index') }}">질의응답</a>
+                    <a class="nav-link" href="#">질의응답</a>
                 </li>
             </ul>
 
@@ -41,11 +41,8 @@
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
-						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-							<a class="dropdown-item" href="#">회원정보수정</a>
-							<a class="dropdown-item" href="{{ route('myarticles.index') }}">내가 쓴글</a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{ route('sessions.destroy') }}">{{ __('Logout') }}</a>
-							<a class="dropdown-item" href="#">회원 관리</a>
                         </div>
                     </li>
                 @endguest
