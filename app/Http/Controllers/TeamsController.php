ef2cdff7117420746aafae0fca037352daa81b23
장현호 <jhh9507@gmail.com>
2019-12-04 13:06:10 +0000

    주훈이꺼  검토 및  회원정보수정 초본

--- app/Http/Controllers/TeamsController.php
+++ app/Http/Controllers/TeamsController.php
@@ -5,58 +5,50 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 class TeamsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('team');
-    }
+	public function index()
+	{
+		return view('team');
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        $teams = \App\Team::with(array('user'=>function($query){
-            $query->select('name','email','id');}))->get(['id','user_id']);
-		if(!auth()->check())
-        	return response()->json([$teams,true]);
+		$teams = \App\Team::with(array('user'=>function($query){
+			$query->select('name','email','id');
+		}))->get(['id','user_id']);
+		
+		if(!auth()->check()){
+			return response()->json([$teams,true]);
+		}
+		
 		$check = \App\Team::where('user_id',auth()->user()->id)->doesntExist();
+		
 		$user = auth()->user();
-		if($check  && ($user->rank === 'B' || $user->rank === 'A'))
-        	return response()->json([$teams,false]);
+		
+		if($check  && ($user->rank === 'B' || $user->rank === 'A')){
+			return response()->json([$teams,false]);
+		}
+		
 		return response()->json([$teams,true]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-		if(!auth()->check())
-        	return;
-		$user = auth()->user();
-		if($user->rank !== 'B' && $user->rank !== 'A')
+		if(!auth()->check()){
 			return;
+		}
+        	
+		$user = auth()->user();
+		
+		if($user->rank !== 'B' && $user->rank !== 'A'){
+			return;
+		}
+			
 		$comment = $request->comment;
         \App\User::find($user->id)->team()->create(['comment'=>$comment]);
+		
         return response()->json(auth()->user());
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $team = \App\User::where('id',$id)->with('team')->get();
--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -11,7 +11,7 @@ class ArticlesTableSeeder extends Seeder
     {	
 		for($i=1;$i<30;$i++) {
 			App\User::find($i)->articles()->create([	
-				'article_type' => '현지학기제용글',
+				'article_type' => 'LS',
 				'title' => '현지학기줴 제목'.$i,
 				'content' => '현지학기제 내용'.$i,
         	]);
