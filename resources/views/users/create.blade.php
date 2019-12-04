d6f0d2f8fb257d2aad56d694c68db5b82dbd8f06
곽 주훈 <hn04193@naver.com>
2019-12-04 13:25:39 +0000

    중2병곽주ㅜ=훈

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -54,63 +54,69 @@
                 <h2 class="">{{ __('Register') }}</h2>
                 <h3 class="">Please edit your membership information.</h3>
             </div>
-			
-            <form action="{{ route('users.store') }}" method="POST">
-                @csrf
-				
-                <div class="form-group text-center">
-                    <span class="fa-stack fa-7x">
-                        <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fas fa-user fa-stack-1x fa-inverse"></i>
-                    </span>
-                    <img class="rounded-circle" id="holder" src="" alt="" >
-                </div>
-                <div class="form-group text-center">
-                    <div class="btn btn-primary" >사진등록<input type="file" style="display: none;"></div>
-                </div>
-				
-                <div class="form-group">
-                    <input type="email" name="email" class="form-control" placeholder="이메일 *" required>
-                </div>
-				
-                <div class="form-group">
-                    <input type="password" name="password" class="form-control" placeholder="비밀번호 *" required>
-                </div>
-				
-                <div class="form-group">
-                    <input type="password" name="password_confirmation" class="form-control" placeholder="비밀번호 확인 *" required>
-                </div>
-				
-                <div class="form-group">
-                    <input type="text" name="name" class="form-control" placeholder="이름 *" required>
-                </div>
-				
-                <div class="form-group birth-group">
-                    <select id="year" name="year" class="form-control birth" required>
-                        <option value="">년(4자)*</option>
-                    </select>
-                    <select id="month" name="month" class="form-control birth" required>
-                        <option value="">월*</option>
-                    </select>
-                    <select id="day" name="day" class="form-control birth" required>
-                        <option value="">일*</option>
-                    </select>
-                </div>
-				
-                <div class="form-group">
-                    <select name="gender" class="form-control" required>
-                        <option value="">성별*</option>
-                        <option value="woman">여</option>
-                        <option value="men">남</option>
-                    </select>
-                </div>
+			@guest
+			<form action="{{ route('users.store') }}" method="POST">
+			@else
+			<form action="{{ route('users.update') }}" method="PUT">
+			@endguest
+				@csrf
 
-				
-                <div class="form-group">
-                    <button class="btn btn-primary btn-block text-uppercase" type="submit">가 입 하 기</button>
-                    {{-- <button class="btn btn-primary btn-block text-uppercase" type="submit">수 정 완 료</button> --}}
-                </div>
-            </form>
+				<div class="form-group text-center">
+					<span class="fa-stack fa-7x">
+						<i class="fas fa-circle fa-stack-2x text-primary"></i>
+						<i class="fas fa-user fa-stack-1x fa-inverse"></i>
+					</span>
+					<img class="rounded-circle" id="holder" src="" alt="" >
+				</div>
+				<div class="form-group text-center">
+					<div class="btn btn-primary" >사진등록<input type="file" style="display: none;"></div>
+				</div>
+
+				<div class="form-group">
+					<input type="email" name="email" class="form-control" placeholder="이메일 *" required>
+				</div>
+
+				<div class="form-group">
+					<input type="password" name="password" class="form-control" placeholder="비밀번호 *" required>
+				</div>
+
+				<div class="form-group">
+					<input type="password" name="password_confirmation" class="form-control" placeholder="비밀번호 확인 *" required>
+				</div>
+
+				<div class="form-group">
+					<input type="text" name="name" class="form-control" placeholder="이름 *" required>
+				</div>
+
+				<div class="form-group birth-group">
+					<select id="year" name="year" class="form-control birth" required>
+						<option value="">년(4자)*</option>
+					</select>
+					<select id="month" name="month" class="form-control birth" required>
+						<option value="">월*</option>
+					</select>
+					<select id="day" name="day" class="form-control birth" required>
+						<option value="">일*</option>
+					</select>
+				</div>
+
+				<div class="form-group">
+					<select name="gender" class="form-control" required>
+						<option value="">성별*</option>
+						<option value="woman">여</option>
+						<option value="men">남</option>
+					</select>
+				</div>
+
+
+				<div class="form-group">
+					@guest
+					<button class="btn btn-primary btn-block text-uppercase" type="submit">가 입 하 기</button>
+					@else
+					<button class="btn btn-primary btn-block text-uppercase" type="submit">수 정 완 료</button>
+					@endguest
+				</div>
+			</form>
         </div>
     </div>
 </div>
